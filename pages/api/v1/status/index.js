baeddf8ed55bb4324058b09450e0e17b0d06ba87
Gabriel Dornelas <zerick.gd@gmail.com>
2024-12-06 22:46:32 +0000

    docker e env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 0 + 0 as sum;");
+  console.log(result);
   response.status(200).json({ chave: "primeira API" });
 }
 
