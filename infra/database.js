983476ecb97802f1cc9f4f16e50782da1910d1b4
Gabriel Dornelas <zerick.gd@gmail.com>
2024-12-10 16:54:07 +0000

    lidando caso certificado requirido

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "development" ? false : process.env.ssl,
+    ssl: getSSLValues(),
   });
-  console.log("Credenciais", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: process.env.ssl,
-  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -32,3 +25,10 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return { ca: process.env.POSTGRES_CA };
+  }
+  return process.env.NODE_ENV === "development" ? false : process.env.ssl;
+}
